93dfd1bf80f8ad1b50f5dc3f28a7f75048680d25
Ayorinde Aadekunle <Ayorinde Aadekunle>
2022-10-14 19:10:18 -0400

    signed commit

---
+++ if.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World!")
+
+	age := 21
+	if age >= 19 {
+		fmt.Println("age is okay", age)
+	} else {
+		fmt.Println("age is not okay", age)
+	}
+
+}
